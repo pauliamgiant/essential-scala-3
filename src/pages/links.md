189334457f7bae19297917b395bc70b4b422175b
Paul Matthews <44086830+pauliamgiant@users.noreply.github.com>
2026-02-03 06:19:53 +0000

    migrate appendiix update getting started

--- src/pages/links.md
+++ src/pages/links.md
@@ -1,12 +1,11 @@
 [scala.Option]: https://www.scala-lang.org/api/current/index.html#scala.Option
 
-[link-advanced-scala]: https://underscore.io/books/advanced-scala
+[link-functional-programming-strategies]: https://functionalprogrammingstrategies.com/
 [link-bnf]: https://en.wikipedia.org/wiki/Backus%E2%80%93Naur_form
 [link-json]: http://www.json.org/
 [link-linked-list]: https://en.wikipedia.org/wiki/Linked_list
 [link-multiple-inheritance]: http://en.wikipedia.org/wiki/Multiple_inheritance
 [link-precedence-rules]: http://stackoverflow.com/questions/2922347/operator-precedence-in-scala
 [link-product-types]: http://en.wikipedia.org/wiki/Product_type
-[link-tut]: https://github.com/tpolecat/tut
+[link-mdoc]: https://github.com/scalameta/mdoc
 [link-uap]: http://en.wikipedia.org/wiki/Uniform_access_principle
-[link-underscore]: https://underscore.io
--- src/pages/pattern-matching/extractors.scala
+++ src/pages/pattern-matching/extractors.scala
@@ -1,39 +1,36 @@
-object Positive {
+object Positive:
   def unapply(in: Int): Option[Int] =
-    if(in > 0)
+    if in > 0 then
       Some(in)
     else
       None
-}
 
-assert(
-  "No" ==
-    (0 match {
-       case Positive(_) => "Yes"
-       case _ => "No"
-     })
-)
+def main(args: Array[String]): Unit =
+  assert(
+    "No" ==
+      (0 match
+        case Positive(_) => "Yes"
+        case _           => "No"),
+  )
 
-assert(
-  "Yes" ==
-    (42 match {
-       case Positive(_) => "Yes"
-       case _ => "No"
-     })
-)
+  assert(
+    "Yes" ==
+      (42 match
+        case Positive(_) => "Yes"
+        case _           => "No"),
+  )
 
-object Titlecase {
-  def unapply(str: String): Option[String] = {
+object Titlecase:
+  def unapply(str: String): Option[String] =
     Some(str.split(" ").toList.map {
-      case "" => ""
+      case ""   => ""
       case word => word.substring(0, 1).toUpperCase + word.substring(1)
     }.mkString(" "))
-  }
-}
 
-assert(
-  "Sir Lord Doctor David Gurnell" ==
-    ("sir lord doctor david gurnell" match {
-       case Titlecase(str) => str
-     })
-)
+end main
+def test1(): Unit =
+  assert(
+    "Sir Lord Doctor David Gurnell" ==
+      ("sir lord doctor david gurnell" match
+        case Titlecase(str) => str),
+  )
